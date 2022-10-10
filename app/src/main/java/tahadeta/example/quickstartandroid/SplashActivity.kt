0da0f9561a1ef144262cf213bbecf94d06298de4
taha jadid <taha.jadid@inwi.ma>
2022-10-10 21:00:10 +0100

    First Commit

---
+++ app/src/main/java/tahadeta/example/quickstartandroid/SplashActivity.kt
@@ -0,0 +1,11 @@
+package tahadeta.example.quickstartandroid
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
